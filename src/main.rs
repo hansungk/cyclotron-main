25f2e7734bfbff5a25fc0f4688fe67adb4116ef0
Hansung Kim <hansung_kim@berkeley.edu>
2024-12-05 11:49:29 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use radpie::sim::Sim;
+
+fn main() {
+    let mut sim = Sim::new();
+
+    for _ in 0..10 {
+        let imem_req = sim.imem_req.get();
+        match imem_req {
+            Some(data) => {
+                println!("detected imem req: data={}", data);
+                assert!(
+                    sim.imem_resp.put(data + 42, sim.time() + 1),
+                    "response put failed!"
+                ); // bogus
+            }
+            None => {}
+        }
+
+        sim.tick();
+    }
+}
